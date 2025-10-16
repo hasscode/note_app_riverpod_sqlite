57359f38e830ee9ee62227d31a917713cf373f4d
hasscode <sona72003@gmail.com>
2025-10-17 02:27:06 +0300

    project archhitecture

---
+++ android/app/src/main/kotlin/com/example/note_app_sqlite_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.note_app_sqlite_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
